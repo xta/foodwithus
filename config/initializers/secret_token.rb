ff4b8a0d44b5f8056a58ffaf90208599591a115f
Rex Feng <rexfeng@gmail.com>
2013-01-20 00:22:57 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodwithus::Application.config.secret_token = '0e0d6927fc6cb92078b2be7436a2413081ee0ceef97b89031f0467e48723f82633d74803dde2a4d67c685448f14ce477ca7cd7f93c41cc0b144c8aab2714ca50'
