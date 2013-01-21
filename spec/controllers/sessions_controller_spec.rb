755e379366509589d58f2ae0297134882bcf3739
Rex Feng <rexfeng@gmail.com>
2013-01-21 13:50:11 -0500

    create session controller for testing

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe SessionsController do
+
+  describe '#create' do
+
+    it 'sets the current user if valid'
+
+    it 'does not set the current user if not valid login'
+
+  end
+
+  describe '#destroy' do
+
+    it 'destroys the current user session'
+
+  end
+
+end
