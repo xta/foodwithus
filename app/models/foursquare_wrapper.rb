0331fb0edb263f22857dd664a1bfcbbec1abf2da
Rex Feng <rexfeng@gmail.com>
2013-01-21 22:12:44 -0500

    add 4sq wrapper files

---
+++ app/models/foursquare_wrapper.rb
@@ -0,0 +1,3 @@
+class FoursquareWrapper
+
+end
---
+++ spec/models/foursquare_wrapper_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe FoursquareWrapper do
+
+  describe '.users_friends'
+
+  describe '.user_venuestats'
+
+  describe '.search_nearby_restaurants'
+
+end
