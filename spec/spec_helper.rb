1dfe7a0521007ef641b031c656743241f90f295e
Rex Feng <rexfeng@gmail.com>
2013-01-23 21:28:11 -0500

    add vcr and webmock to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'webmock/rspec'
+require 'vcr'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -37,4 +39,11 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.hook_into :webmock
+  # c.default_cassette_options = { :record => :new_episodes, :erb => true }
+  c.filter_sensitive_data("<TEST_TOKEN>") { ENV['FOURSQUARE_TEST_TOKEN'] }
 end
