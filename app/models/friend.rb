9a0df4a2b86d4bae06986bf50e0ffb4f91f0fe50
Rex Feng <rexfeng@gmail.com>
2013-01-24 01:21:35 -0500

    add method for friend to build a new friend from foursquare api friend

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,7 +1,32 @@
 class Friend < ActiveRecord::Base
-  attr_accessible :email, :first_name, :homecity, :last_name, :photo_prefix, :photo_suffix, :relationship, :uid, :user_id
+  # attr_accessible :email, :first_name, :homecity, :last_name, :photo_prefix, :photo_suffix, :relationship, :uid, :user_id
 
   belongs_to :user
 
   validates :uid, :presence => true
+
+  def self.build_foursquare_friend(the_user_id, foursquare_api_friend)
+
+    new_friend = self.new
+
+    begin
+
+      new_friend.email        = foursquare_api_friend.contact.email
+      new_friend.first_name   = foursquare_api_friend.firstName
+      new_friend.homecity     = foursquare_api_friend.homeCity
+      new_friend.last_name    = foursquare_api_friend.lastName
+      new_friend.photo_prefix = foursquare_api_friend.photo
+# new_friend.photo_suffix = foursquare_api_friend.
+      new_friend.relationship = foursquare_api_friend.relationship
+      new_friend.uid          = foursquare_api_friend.id
+
+      new_friend.user_id      = the_user_id
+
+    rescue Exception => e
+      logger.debug e
+    end
+
+    return new_friend
+  end
+
 end
--- spec/models/friend_spec.rb
+++ spec/models/friend_spec.rb
@@ -2,4 +2,25 @@ require 'spec_helper'
 
 describe Friend do
 
+  describe '#build_foursquare_friend' do
+
+    before(:each) do
+      @user = FactoryGirl.create(:user, token: ENV['FOURSQUARE_TEST_TOKEN'])
+      client = FoursquareWrapper.new(@user)
+
+      VCR.use_cassette('build_foursquare_friend') do
+        @foursquare_api_friend = client.user_friends.first
+      end
+    end
+
+    it 'builds a Friend from a foursquare_api friend' do
+      new_friend = Friend.build_foursquare_friend(@user.id, @foursquare_api_friend)
+
+      new_friend.instance_of?(Friend).should eql(true)
+      new_friend.first_name.should eql("Tim")
+    end
+
+  end
+  
+
 end
