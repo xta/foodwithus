9f68854c02c134a76d1e872e471d3bd0fd6ae0f9
Rex Feng <rexfeng@gmail.com>
2013-01-30 10:53:00 -0500

    remove group description

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130124045134) do
+ActiveRecord::Schema.define(:version => 20130130154817) do
 
   create_table "friends", :force => true do |t|
     t.integer  "uid"
@@ -29,9 +29,8 @@ ActiveRecord::Schema.define(:version => 20130124045134) do
 
   create_table "groups", :force => true do |t|
     t.string   "name"
-    t.string   "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
