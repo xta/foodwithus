015ed6a45e07cb94c533fc53d8129363b5ca74f2
Rex Feng <rexfeng@gmail.com>
2013-02-02 18:23:34 -0500

    add database_clear

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ require 'rspec/autorun'
 require 'rspec/rails/mocha'
 require 'webmock/rspec'
 require 'vcr'
+require 'database_cleaner'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -40,6 +41,20 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # db cleaner via https://github.com/bmabey/database_cleaner
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
 end
 
 VCR.configure do |c|
