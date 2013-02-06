25462ece15282ceb7be4d465f90b520bc41998e7
Rex Feng <rexfeng@gmail.com>
2013-02-05 22:19:39 -0500

    validate uniqueness of category id

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ActiveRecord::Base
   # attr_accessible :fsq_id, :name, :plural_name, :short_name
 
+  validates :fsq_id, :uniqueness => true
+
   has_many :friend_profiles
   has_many :friends, :through => :friend_profiles
 
