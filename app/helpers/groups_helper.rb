c4844ae8fb1e78fb7cf5e73b96b7a0c39abdf7c7
Rex Feng <rexfeng@gmail.com>
2013-02-19 12:39:08 -0500

    update basic UI with food bg, adjust views

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,5 +1,10 @@
 module GroupsHelper
 
+  def prepopulated_group_name
+    description = ["Food", "Eat", "Party", "Band", "Troop", "Grub", "Snack", "Feast", "Nuggets"].sample
+    "#{description} #{Group.count}"
+  end
+
   def display_top_categories(group)
     group.top_categories.map {|category| category.short_name }.to_sentence
   end
